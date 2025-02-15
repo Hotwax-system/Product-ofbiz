cda8d229f9bdba4ee33c082b6a7bf3c86a6d99f4
GAURAVKADASKAR <kadskargaurav@gmail.com>
2025-02-15 14:14:21 +0530

    Update Product

--- groovyScripts/UpdateProduct.groovy
+++ groovyScripts/UpdateProduct.groovy
@@ -1,23 +1,42 @@
-import org.apache.ofbiz.entity.GenericEntityException;
+import org.apache.ofbiz.entity.Delegator
+import org.apache.ofbiz.entity.GenericValue
+import org.apache.ofbiz.entity.GenericEntityException
+import java.math.BigDecimal
+import java.sql.Timestamp
+import org.apache.ofbiz.entity.DelegatorFactory
+import org.apache.ofbiz.base.util.Debug
+import org.apache.ofbiz.entity.Delegator
 
+def UpdateProductDetail() {
+   
+    Delegator delegator = (Delegator) context.delegator
 
-def UpdatePRoductDetail() {
+    GenericValue productData = delegator.findOne("Product", ["productId": context.productId], false)
+    GenericValue productprice = delegator.findByAnd("ProductPrice", ["productId": context.productId], null,false)?.get(0)
 
-    try{
-        
-        productdata=delegator.findOne("Product",["productId":context.productId],false)
-        if(productdata)
-        {
-            productdata.setNonPKFields(context)
-            delegator.store(productdata)
-        }
-    }
-    catch(GenericEntityException e)
-    {
-        return error("Something went wrong")
+    if (productData && productprice) {
+        productData.set("productName", context.productName)
+        delegator.store(productData)
+
+        productprice.set("thruDate",new Timestamp(System.currentTimeMillis()))
+        delegator.store(productprice)
+
+        GenericValue productPricecreate = delegator.makeValue("ProductPrice")
+        productPricecreate.set("productId", context.productId)
+        productPricecreate.set("productPriceTypeId", "DEFAULT_PRICE")
+        productPricecreate.set("productPricePurposeId", "PURCHASE")
+        productPricecreate.set("currencyUomId", "USD")
+        productPricecreate.set("productStoreGroupId", "_NA_")
+        productPricecreate.set("price", new BigDecimal(context.productPrice ?: 0))
+        productPricecreate.set("fromDate", new Timestamp(System.currentTimeMillis()))
+        delegator.create(productPricecreate)
+
+        return success("Product updated successfully")
     }
     
-    return  "dsdnknd"
-  
+     else {
+        Debug.logError("Product not found for ID: " + context.productId, "UpdateProductDetail")
+        return error("Product not found")
+    }
 }
 
